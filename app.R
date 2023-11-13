bc54e7b3372561b59eec8d9c5cdd794b838551dc
domino-ross-sharp <132300875+domino-ross-sharp@users.noreply.github.com>
2023-11-13 11:33:31 -0700

    Update app.R

--- app.R
+++ app.R
@@ -10,7 +10,7 @@ server <- function(input, output) {
 
 ui <- fluidPage(sidebarLayout(sidebarPanel(
   sliderInput(
-    "obs", "Number of observations:", min = 10, max = 500, value = 100
+    "obs", "Number fgergre of observations:", min = 10, max = 500, value = 100
   )
 ),
 
